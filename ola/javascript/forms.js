43c37387660fac6d1da3a38f8656e6fd5b213fac
PedroLucaspllgc <161877989+PedroLucaspllgc@users.noreply.github.com>
2026-09-02 09:18:52 -0300

    Add files via upload

---
+++ ola/javascript/forms.js
@@ -0,0 +1,13 @@
+function Form(){
+    <div>
+        <h1>Forms</h1>
+        <form onSubmit={cadastrarAlgo}>
+            <div>
+                <input type="text" placeholde="digite alguma coisa"/>
+            </div>
+            <div>
+                <input type="submit" vaue="enviar"/>
+            </div>
+        </form>
+    </div>
+}
---
+++ ola/javascript/main.js
@@ -0,0 +1,13 @@
+function Form(){
+    <div>
+        <h1>Forms</h1>
+        <form>
+            <div>
+                <input type="text" placeholde="digite alguma coisa"/>
+            </div>
+            <div>
+                <input type="submit" vaue="enviar"/>
+            </div>
+        </form>
+    </div>
+}
